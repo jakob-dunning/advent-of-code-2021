e41ca276bc3f5140d10427311586e3d10a2977ce
Jakob Dunning <jakob.dunnning@luckycloud.de>
2021-12-12 17:14:54 +0100

    Added .gitignore, solved puzzle6 part 2

--- puzzle/Day6.php
+++ puzzle/Day6.php
@@ -2,20 +2,32 @@
 
 class Day6
 {
-    private const NUM_DAYS = 256;
+    private const NUM_DAYS_PART1 = 80;
+    private const NUM_DAYS_PART2 = 256;
     private Swarm $swarm;
+    private array $swarmBuckets = [];
 
     public function run()
     {
-        $this->swarm = new Swarm();
         $data = file_get_contents(__DIR__ . '/../input/day6.txt');
+        $data = explode(",", $data);
 
-        foreach (explode(",", $data) as $timer) {
+        $this->part1($data);
+
+        echo '###';
+
+        $this->part2($data);
+    }
+
+    private function part1(array $data): void
+    {
+        $this->swarm = new Swarm();
+
+        foreach ($data as $timer) {
             $this->swarm->add(new Fish((int)$timer));
         };
 
-        for ($i = 0; $i < self::NUM_DAYS; $i++) {
-            echo "$i\n";
+        for ($i = 0; $i < self::NUM_DAYS_PART1; $i++) {
             /** @var Fish $fish */
             foreach ($this->swarm as $fish) {
                 $fish->decreaseTimer();
@@ -26,6 +38,24 @@ class Day6
         }
 
         echo count($this->swarm);
+    }
+
+    private function part2(array $data)
+    {
+        $this->swarmBuckets = array_fill(0, 9, 0);
+
+        foreach ($data as $timer) {
+            $this->swarmBuckets[$timer]++;
+        }
+
+        for ($i = 0; $i < self::NUM_DAYS_PART2; $i++) {
+            $spawningFishCount = array_shift($this->swarmBuckets);
+
+            $this->swarmBuckets[6] += $spawningFishCount;
+            $this->swarmBuckets[8] = $spawningFishCount;
+        }
+
+        echo array_sum($this->swarmBuckets);
     }
 }
 
